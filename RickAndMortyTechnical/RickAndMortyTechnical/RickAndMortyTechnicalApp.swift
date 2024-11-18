af82fd9e248ac5a801e04e5d9f4f68e4884af571
Joan Cremades <joancm@ext.inditex.com>
2024-11-18 15:58:32 +0100

    First Commit

---
+++ RickAndMortyTechnical/RickAndMortyTechnical/RickAndMortyTechnicalApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTechnicalApp.swift
+//  RickAndMortyTechnical
+//
+//  Created by Joan Cremades Meló on 18/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyTechnicalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
